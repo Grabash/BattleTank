76aa23dce6b7e94e4bbd50655912bbb4461346d2
PM <milewski.przemek@gmail.com>
2019-02-21 12:44:30 +0100

    S4L217 Setting SprungWheel BP in C++

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,42 @@
+// Copyright Orajt
+
+#include "SprungWheel.h"
+#include "Engine/StaticMesh.h"
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
+	SetRootComponent(Mass);
+
+	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
+	// Wheel->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	// or, possible in the constructor only?
+	Wheel->SetupAttachment(Mass);
+
+	MassWheelConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Mass Wheel Constraint"));
+	// MassWheelConstraint->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	MassWheelConstraint->SetupAttachment(Mass);
+
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
